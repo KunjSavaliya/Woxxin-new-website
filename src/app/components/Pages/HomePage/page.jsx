aec8caabeca5bf40e466f54149b502e1c3183706
KunjSavaliya <kunj.bvminfotech@gmail.com>
2025-05-04 17:44:45 +0530

    add new code

--- src/app/components/Pages/HomePage/page.jsx
+++ src/app/components/Pages/HomePage/page.jsx
@@ -28,67 +28,69 @@ function MainPage() {
           <div className="flex gap-80 lg:px-16 xl:px-32">
             <div className="flex flex-col mt-32 sm:mt-40 text-white gap-4 sm:gap-7 xl:gap-12 p-3 sm:px-12 md:px-24 lg:px-16 xl:px-36">
 
-              {/* <ScrollAnimation animateIn="animate__animated animate__fadeInRight"> */}
-              <Text className="text-5xl md:text-5xl lg:text-5xl xl:text-[95px] font-bold text-white text-gradient text-start ">
-                Accelerate Your App / H5 Game Growth
-              </Text>
-              {/* </ScrollAnimation> */}
-              {/* <ScrollAnimation animateIn="animate__animated animate__fadeInLeft"> */}
+              <ScrollAnimation animateIn="animate__animated animate__fadeInUp">
 
-              <Text className="text-[20px] md:text-2xl lg:text-2xl xl:text-[30px] text-white font-medium text-start ">
-                Woxxin Solutions is a dynamic company focused on creating innovative Mobile Applications and engaging Mobile games, boasting over 650 million downloads globally.
-              </Text>
-              {/* </ScrollAnimation> */}
-              {/* <ScrollAnimation animateIn=" animate__animated animate__fadeInUp"> */}
+                <Text className="text-5xl md:text-5xl lg:text-5xl xl:text-[95px] font-bold text-white text-gradient text-start ">
+                  Accelerate Your App / H5 Game Growth
+                </Text>
+              </ScrollAnimation>
+              <ScrollAnimation animateIn="animate__animated animate__fadeInUp">
 
-              <Text className="text-xl md:text-2xl lg:text-3xl xl:text-[25px] text-[#ffffff]">
-                PUBLISHER
-              </Text>
+                <Text className="text-[20px] md:text-2xl lg:text-2xl xl:text-[30px] text-white font-medium text-start ">
+                  Woxxin Solutions is a dynamic company focused on creating innovative Mobile Applications and engaging Mobile games, boasting over 650 million downloads globally.
+                </Text>
+              </ScrollAnimation>
+              <ScrollAnimation animateIn="animate__animated animate__fadeInUp">
 
-              {/* Image with fadeInUp */}
-              {/* <img
+
+                <Text className="text-xl md:text-2xl lg:text-3xl xl:text-[25px] text-[#ffffff]">
+                  PUBLISHER
+                </Text>
+
+                {/* Image with fadeInUp */}
+                <img
                   src="/HomePages/line.png"
                   alt="Advertisers Icon"
-                  className="animate__animated animate__fadeInUp"
-                /> */}
+                  className="" />
 
-              {/* Stats Blocks with fadeInUp */}
-              <div className="flex flex-wrap md:flex-nowrap justify-center divide-y md:divide-y-0 md:divide-x divide-[#9387FF] mb-[100px] ">
-                {/* Block 1 */}
-                <div className="flex flex-col w-full md:w-auto max-w-[300px] px-6 py-6 text-start space-y-2">
-                  <Text className="text-xl md:text-2xl lg:text-3xl xl:text-[35px] text-white">
-                    Top 20
-                  </Text>
-                  <Text className="text-base md:text-xl lg:text-xl xl:text-[20px] text-white font-medium break-words">
-                    Global Google Play Publisher in Non-Game
-                  </Text>
+                {/* Stats Blocks with fadeInUp */}
+                <div className="flex flex-wrap md:flex-nowrap justify-center divide-y md:divide-y-0 md:divide-x divide-[#9387FF] mb-[100px] ">
+                  {/* Block 1 */}
+                  <div className="flex flex-col w-full md:w-auto max-w-[300px] px-6 py-6 text-start space-y-2">
+                    <Text className="text-xl md:text-2xl lg:text-3xl xl:text-[35px] text-white">
+                      Top 20
+                    </Text>
+                    <Text className="text-base md:text-xl lg:text-xl xl:text-[20px] text-white font-medium break-words">
+                      Global Google Play Publisher in Non-Game
+                    </Text>
+                  </div>
+
+                  {/* Block 2 */}
+                  <div className="flex flex-col w-full md:w-auto max-w-[300px] px-6 py-6 text-start space-y-2">
+                    <Text className="text-xl md:text-2xl lg:text-3xl xl:text-[35px] text-white">
+                      Top 03
+                    </Text>
+                    <Text className="text-base md:text-xl lg:text-xl xl:text-[20px] text-white font-medium break-words">
+                      In Southeast Asia Non-Game segment
+                    </Text>
+                  </div>
+
+                  {/* Block 3 */}
+                  <div className="flex flex-col w-full md:w-auto max-w-[300px] px-6 py-6 text-start space-y-2">
+                    <Text className="text-xl md:text-2xl lg:text-3xl xl:text-[35px] text-white">
+                      2022
+                    </Text>
+                    <Text className="text-base md:text-xl lg:text-xl xl:text-[20px] text-white font-medium break-words">
+                      Outstanding Company in Business Development and Expansion by Google
+                    </Text>
+
+                  </div>
+
                 </div>
-
-                {/* Block 2 */}
-                <div className="flex flex-col w-full md:w-auto max-w-[300px] px-6 py-6 text-start space-y-2">
-                  <Text className="text-xl md:text-2xl lg:text-3xl xl:text-[35px] text-white">
-                    Top 03
-                  </Text>
-                  <Text className="text-base md:text-xl lg:text-xl xl:text-[20px] text-white font-medium break-words">
-                    In Southeast Asia Non-Game segment
-                  </Text>
-                </div>
-
-                {/* Block 3 */}
-                <div className="flex flex-col w-full md:w-auto max-w-[300px] px-6 py-6 text-start space-y-2">
-                  <Text className="text-xl md:text-2xl lg:text-3xl xl:text-[35px] text-white">
-                    2022
-                  </Text>
-                  <Text className="text-base md:text-xl lg:text-xl xl:text-[20px] text-white font-medium break-words">
-                    Outstanding Company in Business Development and Expansion by Google
-                  </Text>
-
-                </div>
-
-              </div>
-              {/* </ScrollAnimation> */}
+              </ScrollAnimation>
 
             </div>
+
           </div>
         </div>
 
@@ -117,17 +119,17 @@ function MainPage() {
 
             <div className='mt-5 contents'>
               <div className='contents'>
-                {/* <ScrollAnimation animateIn="animate__animated animate__fadeInUp"> */}
+                <ScrollAnimation animateIn="animate__animated animate__fadeInUp">
 
-                <div className='animate__animated animate__fadeInUp'>
-                  <Text size="text-[45px] md:text-[65px] lg:text-[65px] xl:text-[115px] font-bold text-white text-gradient text-center text2-gradient">
-                    1 Bil
-                  </Text>
-                  <Text size='text-xl md:text-2xl lg:text-3xl xl:text-[45px] text-white'>
-                    Global Downloads
-                  </Text>
-                </div>
-                {/* </ScrollAnimation> */}
+                  <div className='animate__animated animate__fadeInUp'>
+                    <Text size="text-[45px] md:text-[65px] lg:text-[65px] xl:text-[115px] font-bold text-white text-gradient text-center text2-gradient">
+                      1 Bil
+                    </Text>
+                    <Text size='text-xl md:text-2xl lg:text-3xl xl:text-[45px] text-white'>
+                      Global Downloads
+                    </Text>
+                  </div>
+                </ScrollAnimation>
                 <img
                   src="/HomePages/Appimg.png"
                   alt="Advertisers Icon"
@@ -135,18 +137,19 @@ function MainPage() {
                 />
               </div>
             </div>
-            {/* <ScrollAnimation animateIn="animate__animated animate__fadeInUp"> */}
+            <ScrollAnimation animateIn="animate__animated animate__fadeInUp">
 
 
-            <div className='animate__animated animate__fadeInUp'>
-              <Text size="text-center text-[45px] md:text-[65px] lg:text-[[65px] xl:text-[115px] font-bold text-white ActiveUser-gradient text-center text2-gradient">
-                50M+
-              </Text>
-              <Text size='text-xl md:text-2xl lg:text-3xl xl:text-[26px] text-white '>
-                Monthly Active Users
-              </Text>
-            </div>
-            {/* </ScrollAnimation> */}
+
+              <div className='animate__animated animate__fadeInUp'>
+                <Text size="text-center text-[45px] md:text-[65px] lg:text-[[65px] xl:text-[115px] font-bold text-white ActiveUser-gradient text-center text2-gradient">
+                  50M+
+                </Text>
+                <Text size='text-xl md:text-2xl lg:text-3xl xl:text-[26px] text-white '>
+                  Monthly Active Users
+                </Text>
+              </div>
+            </ScrollAnimation>
           </div>
 
         </div>
@@ -233,21 +236,22 @@ function MainPage() {
               {/* <ScrollAnimation animateIn="animate__animated animate__fadeInRight"> */}
 
               <div className="w-full md:w-2/5 mt-6 md:mt-0 ">
-                {/* <ScrollAnimation animateIn="animate__animated animate__fadeInRight"> */}
-                <ul className="space-y-4">
-                  {[
-                    "Market Research",
-                    "Marketing Optimization",
-                    "Product Optimization",
-                    "Monetization/Subscription Optimization",
-                  ].map((item, index) => (
-                    <li key={index} className="flex items-start space-x-3">
-                      <span className="mt-2 h-2 w-2 rounded-full bg-[#AAB0C0] opacity-80"></span>
-                      <span className="text-sm sm:text-base md:text-lg lg:text-[20px]">{item}</span>
-                    </li>
-                  ))}
-                </ul>
-                {/* </ScrollAnimation> */}
+                <ScrollAnimation animateIn="animate__animated animate__fadeInUp">
+
+                  <ul className="space-y-4">
+                    {[
+                      "Market Research",
+                      "Marketing Optimization",
+                      "Product Optimization",
+                      "Monetization/Subscription Optimization",
+                    ].map((item, index) => (
+                      <li key={index} className="flex items-start space-x-3">
+                        <span className="mt-2 h-2 w-2 rounded-full bg-[#AAB0C0] opacity-80"></span>
+                        <span className="text-sm sm:text-base md:text-lg lg:text-[20px]">{item}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollAnimation>
               </div>
               {/* </ScrollAnimation> */}
             </div>
